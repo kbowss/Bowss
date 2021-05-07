aca30531a10b1396913a25bc35ceccec6a592e88
Kareem Al-khaldi <kareem88@hotmail.co.uk>
2021-05-07 11:29:50 +0100

    Initial Commit

---
+++ Bowss/BowssApp.swift
@@ -0,0 +1,17 @@
+//
+//  BowssApp.swift
+//  Bowss
+//
+//  Created by Kareem Al-khaldi on 07/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct BowssApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
